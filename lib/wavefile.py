3574fde77e229bfdee1359d97dc05ae5f72e2557
s-n-1-0 <72431055+s-n-1-0@users.noreply.github.com>
2021-06-26 00:49:19 +0900

    wav書き込み関数追加 lib/wavefile

--- lib/wavefile.py
+++ lib/wavefile.py
@@ -1,8 +1,18 @@
 import wave
 import numpy as np
+from scipy.io.wavfile import write
+def normalize(w):
+    w_max = np.max(w)
+    w_min = np.min(w)
+    return (w - w_min) / (w_max - w_min)
 
-def load_wav(path):
+def read_wav(path):
     print("ファイルチェックはしません。読み込めるファイルについてはReadme.mdを参照してください。")
     w = wave.open(path, "r" )
     buf = w.readframes(w.getnframes())
-    return np.frombuffer(buf, dtype="int16")
+    return w.getnframes(), np.frombuffer(buf, dtype="int16")
+
+def write_wav(path,fs,wav):
+    data  = normalize(wav)*2 - 1
+    data = np.array([data * 32767.0],dtype = "int16")[0]
+    write(path, fs, data)
