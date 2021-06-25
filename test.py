7234358b677fabbd66fba7d94720d58f5c831c2d
s-n-1-0 <72431055+s-n-1-0@users.noreply.github.com>
2021-06-25 23:37:21 +0900

    元に戻す処理とその表示の追加

--- test.py
+++ test.py
@@ -7,7 +7,7 @@ def add_subplot():
     global pcount
     global fig
     pcount += 1
-    return fig.add_subplot(2,2,pcount)
+    return fig.add_subplot(3,2,pcount)
 
 path = "D:/Waves/a.wav"
 fname = "a"
@@ -24,6 +24,7 @@ plt.title("Source : " + fname)
 
 #短時間フーリエ変換(スペクトログラム)表示
 f, t, aspec = signal.stft(a, fs,window=('hamming'),nfft=nfft) 
+print(aspec.shape)
 fx = add_subplot()
 m = fx.pcolormesh(t, f, np.log(np.abs(aspec)) * 10)
 plt.ylabel('Hz')
@@ -43,7 +44,22 @@ x = fft(np.log(np.mean(aspec,axis=1))) #複素数のまま流す
 ans = np.exp(ifft(x))
 fx = add_subplot()
 fx.plot(f,np.log(np.abs(ans))*10,label='linear')
+print((ans).size)
+anspec = np.repeat(ans[:, None], 174, axis=1)
+print(anspec.shape)
 plt.title("mean-UNC")
+fx = add_subplot()
+m = fx.pcolormesh(t, f, np.log(np.abs(anspec)) * 10)
+plt.ylabel('Hz')
+plt.xlabel('s')
+cbar = plt.colorbar(m)
+cbar.ax.set_ylabel("dB")
+plt.title("Spec")
+fx = add_subplot()
+t,result = signal.istft(aspec, fs,window=('hamming'),nfft=nfft)
+fx.plot(result)
+plt.title("Result: " + fname)
+print(result[0])
 fig.tight_layout()
 plt.show()
 
